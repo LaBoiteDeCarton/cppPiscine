5308e0fc467c6b079c5cd0455b4ec0c15747573a
Daniel Mercadiel <dmercadi@c2r2p6.42nice.fr>
2022-06-01 13:32:15 +0200

    first commit

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	int i;
+	int j;
+
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	i = 1;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+			std::cout << (char)toupper(av[i][j++]);
+		std::cout << '\n';
+		i++;	
+	}
+	return (0);
+}
